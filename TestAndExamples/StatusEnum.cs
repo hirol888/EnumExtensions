abe62f3b545fdc4a7d09d76cc39b85427cfa637f
Huining Li <huining.li@fsoft.net.au>
2022-01-14 10:51:13 +1000

    Add project files.

---
+++ TestAndExamples/StatusEnum.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace TestAndExamples
+{
+  public enum StatusEnum
+  {
+    [StatusEnumMetadata(Code = "200",
+      Description =
+        "OK indicates that the request succeeded and that the requested information is in the response. This is the most common status code to receive.",
+      Color = "#69f0ae", Order = 1)]
+    OK,
+
+    [StatusEnumMetadata(Code = "201",
+      Description = "Accepted indicates that the request has been accepted for further processing.", Color = "#00e676",
+      Order = 3)]
+    Accepted,
+
+    [StatusEnumMetadata(Code = "300",
+      Description =
+        "Ambiguous indicates that the requested information has multiple representations. The default action is to treat this status as a redirect and follow the contents of the Location header associated with this response. Ambiguous is a synonym for MultipleChoices.",
+      Color = "#00c853", Order = 2)]
+    Ambiguous,
+
+    [StatusEnumMetadata(Code = "502",
+      Description =
+        "BadGateway indicates that an intermediate proxy server received a bad response from another proxy or the origin server.",
+      Order = 5)]
+    BadGateway,
+
+    [StatusEnumMetadata(Code = "400",
+      Description =
+        "BadRequest indicates that the request could not be understood by the server. BadRequest is sent when no other error is applicable, or if the exact error is unknown or does not have its own error code.",
+      Color = "#7c27b0")]
+    BadRequest
+  }
+
+  public class StatusEnumMetadata : Attribute
+  {
+    public string Code { get; set; }
+    public string Description { get; set; }
+    public string Color { get; set; }
+    public int Order { get; set; }
+  }
+}
---
+++ TestAndExamples/StatusEnumTests.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using EnumExtensions;
+
+namespace TestAndExamples
+{
+  public class StatusEnumTests
+  {
+    [Test]
+    public void Should_Get_All_Metadata()
+    {
+      StatusEnum okStatus = StatusEnum.OK;
+      var code = okStatus.GetAttributeOfType<StatusEnumMetadata>()?.Code;
+      var description = okStatus.GetAttributeOfType<StatusEnumMetadata>()?.Description;
+      var color = okStatus.GetAttributeOfType<StatusEnumMetadata>()?.Color;
+      var order = okStatus.GetAttributeOfType<StatusEnumMetadata>()?.Order;
+
+      Assert.AreEqual("200", code);
+      Assert.AreEqual("OK indicates that the request succeeded and that the requested information is in the response. This is the most common status code to receive.", description);
+      Assert.AreEqual("#69f0ae", color);
+      Assert.AreEqual(1, order);
+    }
+
+    [Test]
+    public void Should_Get_Default_Value_For_Int_If_Not_Provided()
+    {
+      StatusEnum badRequest = StatusEnum.BadRequest;
+      var order = badRequest.GetAttributeOfType<StatusEnumMetadata>()?.Order;
+
+      Assert.AreEqual(0, order);
+    }
+
+    [Test]
+    public void Should_Get_Null_For_String_If_Not_Provided()
+    {
+      StatusEnum badGateway = StatusEnum.BadGateway;
+      var color = badGateway.GetAttributeOfType<StatusEnumMetadata>()?.Color;
+
+      Assert.IsNull(color);
+    }
+
+    [Test]
+    public void Should_Get_Nulls()
+    {
+      StatusEnum? nullStatus = null;
+      var code = nullStatus.GetAttributeOfType<StatusEnumMetadata>()?.Code;
+      var description = nullStatus.GetAttributeOfType<StatusEnumMetadata>()?.Description;
+      var color = nullStatus.GetAttributeOfType<StatusEnumMetadata>()?.Color;
+      var order = nullStatus.GetAttributeOfType<StatusEnumMetadata>()?.Order;
+
+      Assert.IsNull(code);
+      Assert.IsNull(description);
+      Assert.IsNull(color);
+      Assert.IsNull(order);
+    }
+  }
+}
